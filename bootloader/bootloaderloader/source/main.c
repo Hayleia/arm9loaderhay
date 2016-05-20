fc9d667008c410aa079956d8e5b7d52341dac600
hartmannaf <hartmannaf@googlemail.com>
2016-05-20 15:11:37 +0200

    arm9bootloader.bin is now included in arm9loaderhax.bin

--- bootloader/bootloaderloader/source/main.c
+++ bootloader/bootloaderloader/source/main.c
@@ -7,30 +7,25 @@
 #define PAYLOAD_SIZE		0x00100000
 
 FATFS fs;
+extern u8 arm9bootloader_bin[];
+extern u32 arm9bootloader_bin_size;
 
-void loadAndRunPayload(const char* payloadName, u32 payloadAddress)
+void fastmempy(u32* destination, u32* source, u32 size)
 {
-	FIL payload;
-	u32 br;
-	if(f_open(&payload, payloadName, FA_READ | FA_OPEN_EXISTING) == FR_OK)
+	size/=4;
+	while(size--)
 	{
-		f_read(&payload, (void*)payloadAddress, PAYLOAD_SIZE, (UINT*)&br);
-        f_close(&payload);
-		f_mount(&fs, "0:", 1);
-		((void (*)())payloadAddress)();
+		*destination=*source;
+		destination++;
+		source++;
 	}
 }
 
 int main()
 {
-	if(f_mount(&fs, "0:", 1) == FR_OK)
-	{
-		loadAndRunPayload("arm9loaderhax/arm9bootloader.bin", BOOTLOADER_PAYLOAD_ADDRESS);
-		loadAndRunPayload("a9lh/arm9bootloader.bin", BOOTLOADER_PAYLOAD_ADDRESS);
-		loadAndRunPayload("arm9bootloader/arm9bootloader.bin", BOOTLOADER_PAYLOAD_ADDRESS);
-		loadAndRunPayload("arm9bootloader.bin", BOOTLOADER_PAYLOAD_ADDRESS);
-	}
-	
+	fastmempy((u32*)BOOTLOADER_PAYLOAD_ADDRESS,(u32*)arm9bootloader_bin, arm9bootloader_bin_size);
+	((void (*)())BOOTLOADER_PAYLOAD_ADDRESS)();
+
 	i2cWriteRegister(I2C_DEV_MCU, 0x20, (u8)(1<<0));
     return 0;
 }
